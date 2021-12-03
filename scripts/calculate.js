d839980026716631af6d04f3282644088681843e
zmuranaka <zmuranaka@gmail.com>
2021-12-03 14:28:39 -0700

    More testing dark theme for graph

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -51,10 +51,10 @@ function drawGraph(hAxisMin, hAxisTitle, arr, discrete)
     if (graph) graph.clearChart();
     graph = discrete ? new google.visualization.SteppedAreaChart(calculateResultsFig) : new google.visualization.AreaChart(calculateResultsFig);
     let options = {
-        backgroundColor: theme === "dark" ? "#222" : "white",
+        backgroundColor: theme === "dark" ? "#444" : "white",
         legend: {position: "none"},
-        hAxis: {gridlines: {color: theme === "dark" ? "white" : "#ccc"}, title: hAxisTitle, viewWindow: {min: hAxisMin - (discrete ? 1 : 0)}},
-        vAxis: {gridlines: {color: theme === "dark" ? "white" : "#ccc"}, title: `Probability${discrete ? "" : " Density"}`}
+        hAxis: {gridlines: {color: theme === "dark" ? "white" : "#ccc"}, textStyle: {color: theme === "dark" ? "#eee" : "black"}, title: hAxisTitle, viewWindow: {min: hAxisMin - (discrete ? 1 : 0)}},
+        vAxis: {gridlines: {color: theme === "dark" ? "white" : "#ccc"}, textStyle: {color: theme === "dark" ? "#eee" : "black"}, title: `Probability${discrete ? "" : " Density"}`}
     };
     graph.draw(google.visualization.arrayToDataTable(arr), options);
 }
